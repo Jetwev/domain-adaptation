4b00c3f19e894556447893c9a281ce666fb1fb27
Artem <bitiutskii.ad@phystech.edu>
2023-04-21 16:33:53 +0200

    change disriminator

--- models/dann.py
+++ models/dann.py
@@ -71,7 +71,14 @@ class Discriminator(nn.Module):
         self.discriminator = nn.Sequential(
             nn.Linear(in_features=2048, out_features=1024),
             nn.ReLU(),
-            nn.Linear(in_features=1024, out_features=2)
+            nn.Dropout(0.5),
+            nn.Linear(in_features=1024, out_features=256),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(in_features=256, out_features=32),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(in_features=32, out_features=2)
         )
 
     def forward(self, x, alpha):
